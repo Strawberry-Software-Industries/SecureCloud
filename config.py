2d2ed26cc5e5a2e664e1d82a4048b2e1e21717c7
Julian <81520713+Juliandev02@users.noreply.github.com>
2022-03-16 21:48:28 +0100

    Added "Do not change this" to developer_key (comment)

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ ip_type = "networking" # Change between localhost and networking
 global_port = "80" # Port for SecureCloud
 
 # Developer
-developer_key = "xdev_JRkz1Z4UrsExIuFOglLBwpK6ENMYs6_tkey"
+developer_key = "xdev_JRkz1Z4UrsExIuFOglLBwpK6ENMYs6_tkey" # Do not change this!
