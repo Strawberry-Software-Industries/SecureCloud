50177207911b8866aebadff223dcffb39885eca7
Julian <81520713+Juliandev02@users.noreply.github.com>
2022-03-16 22:52:22 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -29,11 +29,11 @@ app.config["SECRET_KEY"] = "xpub_hqFFnmKE7cHe5DhIxdE3_default"
 
 
 # Variables
-release_github = "https://github.com/Strawberry-Software-Industries/SecureCloud/releases/tag/v1.5.2"
+release_github = "https://github.com/Strawberry-Software-Industries/SecureCloud/releases/tag/v1.5.3"
 build_date = "2022-16-03_21-09-45"
-build_ver = "1.6.0_" + build_date
-version_full = "Version 1.6.0"
-version_short = "v1.6.0"
+build_ver = "1.5.3_" + build_date
+version_full = "Version 1.5.3"
+version_short = "v1.5.3"
 revision = "rev-1"
 codename = "Strawberry Mix"
 
